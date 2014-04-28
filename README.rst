b06e581cbff27393ae8b94b9f07ef62c804b5e4e
Mathias Fussenegger <f.mathias@zignar.net>
2014-04-29 00:14:54 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,5 @@
+
+A testlayer for Tornado that can be used with zope.testrunner
+
+
+See _`tornado_testing/layer.rst` for more information.
---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(
+    name='tornado_testing',
+    extras_require={
+        'test': ['tornado']
+    },
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
